126b810fcbeb51f6c4aa74e6c1d8ba3b412ef05c
sgryu01 <sgryu01@qr-bank.co.kr>
2025-04-30 10:19:11 +0900

    📝 docs: 환경별 YAML 설정 및 local 구동법 README에 추가
    🔧 config: application.yaml에서 hibernate.dialect 설정 제거
    
    🧹 chore: application-local.yaml에서 민감정보 제거 및 예시값 비움

--- module-unionpay/src/main/java/com/qrroad/oqms/unionpay/config/ApplicationContextConfig.java
+++ module-unionpay/src/main/java/com/qrroad/oqms/unionpay/config/ApplicationContextConfig.java
@@ -5,11 +5,11 @@ import com.qrroad.oqms.infrastructure.service.VaultService;
 import com.qrroad.oqms.domain.config.DomainConfig;
 import com.qrroad.oqms.infrastructure.config.DBConfig;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 import java.util.Map;
@@ -22,33 +22,37 @@ import java.util.Map;
  *   해당 빈들을 사용할 수 있도록 합니다.
  * <p>
  * 애플리케이션에 필요한 도메인, 인프라 빈을 모두 포함하는 설정 파일입니다.
+ * <p>
+ * <b>주의:</b> local 환경에서 실행할 경우 application-local.yaml에
+ * 반드시 spring.datasource의 모든 항목(url, username, password 등)을 포함해야 합니다.
+ * (local에서는 Spring Boot의 자동설정으로 DataSource가 생성됩니다)
  */
 @Configuration
 @Import({DomainConfig.class, DBConfig.class, VaultConfig.class})
 @Slf4j
 public class ApplicationContextConfig {
-    // 애플리케이션에 필요한 도메인 및 인프라 설정을 모은 클래스
     private final VaultService vaultService;
 
     public ApplicationContextConfig(VaultService vaultService) {
         this.vaultService = vaultService;
     }
 
-    @ConfigurationProperties(prefix = "spring.datasource")
     @Bean
+    @Profile("!local")
     public DataSource dataSource() {
         Map<String, Object> mysqlData = vaultService.getSecretData("/oqms/data/mysql");
-
         String userName = vaultService.decryptByDek(mysqlData.get("username").toString());
         String userPassword = vaultService.decryptByDek(mysqlData.get("userpassword").toString());
+        String url = vaultService.decryptByDek(mysqlData.get("url").toString());
 
         log.info("userName: {}", userName);
         log.info("userPassword: {}", userPassword);
+        log.info("url: {}", url);
 
-        return DataSourceBuilder.create()
-                .username("sgryu01")
-                .password("Qwer1324!@")
-                .url("jdbc:mysql://10.47.4.10:43306/qrbgw?useSSL=false&allowPublicKeyRetrieval=true")
+        return org.springframework.boot.jdbc.DataSourceBuilder.create()
+                .username(userName)
+                .password(userPassword)
+                .url(url)
                 .driverClassName("com.mysql.cj.jdbc.Driver")
                 .build();
     }
